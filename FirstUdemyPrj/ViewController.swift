c2135eca0a094de0d0d07b0c35618d05d2ac606c
flavio70 <flavio.ippolito@gmail.com>
2020-05-01 20:21:58 +0200

    Initial Commit

---
+++ FirstUdemyPrj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstUdemyPrj
+//
+//  Created by Flavio Ippolito on 01/05/2020.
+//  Copyright © 2020 Flavio Ippolito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
